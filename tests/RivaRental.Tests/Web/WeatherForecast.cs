6f0a014e3c4dd157d998faf30cc5ddc01fd6f12e
Jakob Ehn <jakob.ehn@activesolution.se>
2025-04-05 17:33:30 +0200

    initial commit

--- tests/RivaRental.Tests/Web/WeatherForecast.cs
+++ tests/RivaRental.Tests/Web/WeatherForecast.cs
@@ -10,10 +10,12 @@ namespace RivaRental.Tests.Web
         {
             var client = new WebApplicationFactory<Program>()
                             .CreateClient();
-
+           
             var result = await client.GetAsync("/weatherforecast");
 
             Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+            //hello
         }
     }
 }
